74589508b2c00d1da7416bcb75983132d81a918b
Eryn Wells <eryn@erynwells.me>
2018-03-25 21:00:51 -0400

    Rename a test

--- src/b64.rs
+++ src/b64.rs
@@ -86,7 +86,7 @@ mod tests {
     }
 
     #[test]
-    fn cryptopals_input() {
+    fn cryptopals() {
         let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         let ex_output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
         println!("");
